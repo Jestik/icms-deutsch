2e9043a02b8e548097f9f5fd946caed47ed31afd
Jestik <alexey.hacko@gmail.com>
2025-01-30 18:34:33 +0100

    Create list.php

---
+++ package/system/languages/de/controllers/activity/widgets/list.php
@@ -0,0 +1,5 @@
+<?php
+
+    define('LANG_WD_ACTIVITY_LIST_DATASET',         'Feed für die Anzeige');
+    define('LANG_WD_ACTIVITY_LIST_SHOW_AVATARS',    'Avatare anzeigen');
+    define('LANG_WD_ACTIVITY_LIST_DATE_GROUP',      'Nach Datum gruppieren');
